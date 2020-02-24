899448acb9f758c9d4f43d0b0fbc1c12f8064d18
Thomas-Deasington <t.deasington@gmail.com>
2020-02-24 17:25:00 +0100

    bientot image

---
+++ blog/app/uploaders/picture_uploader.rb
@@ -0,0 +1,25 @@
+class PictureUploader < CarrierWave::Uploader::Base
+  # Include RMagick or MiniMagick support:
+  # include CarrierWave::RMagick
+  include CarrierWave::MiniMagick
+
+  # Choose what kind of storage to use for this uploader:
+  storage :file
+  # storage :fog
+
+  # Override the directory where uploaded files will be stored.
+  # This is a sensible default for uploaders that are meant to be mounted:
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def extension_whitelist
+    %w(jpg jpeg gif png)
+  end
+
+  # Create different versions of your uploaded files:
+  version :thumb do
+    process resize_to_fit: [300, 300]
+  end
+  
+end
