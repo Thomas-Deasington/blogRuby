0aee7b06ffc7575afe122c2233fe674cce914d37
Thomas Deasington <t.deasington@gmail.com>
2020-03-01 18:54:31 +0100

    fonctionnel

--- blog/app/models/user.rb
+++ blog/app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  belongs_to :article
+  # belongs_to :article
+  has_many :article
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
